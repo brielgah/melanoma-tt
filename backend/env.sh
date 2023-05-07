a87b92d172fa21c3bf920d7350269ffbd3d5084e
brielgah <root@DESKTOP-M2S4RS6.localdomain>
2023-05-07 02:19:53 -0600

    Adding environment varibales needed for the project

---
+++ backend/env.sh
@@ -0,0 +1,3 @@
+# This file contains the environment variables needed for the project. Please change with the ones corresponding to your environment
+PYTHONPATH='/your_directory/melanoma-tt/backend'
+DATASET_PATH='your_directory'
