fdda4db7d57b0857dab704e4bdc07c1f28d73a59
brielgah <gabrielgh@live.com.mx>
2023-05-09 02:15:56 -0600

    Add modification to custom dataset script

--- backend/cnn/create_dataset.py
+++ backend/cnn/create_dataset.py
@@ -3,7 +3,9 @@ import os
 import cv2
 import csv
 import random
+from torch.utils.data import Dataset
 from image_processing.image import IMG_SIZE,ImageMetadata
+from sklearn.model_selection import train_test_split
 
 class Image():
   
@@ -13,14 +15,17 @@ class Image():
     
   def __str__(self):
     return f"name: {self.metadata.name} melanoma:{'Y' if self.metadata.is_melanoma else 'N'}" 
-
+  
 # Constants 
 DATASET_PATH = os.getenv('DATASET_PATH')
-
+TRAIN_CSV_FILE = os.getenv('TRAIN_CSV')
+TEST_CSV_FILE = os.getenv('TEST_CSV')
+HEADER_CSV = ["name", "path", "melanoma"]
 # Globals
 dataset = []
 
 def create_image_object(img,is_melanoma):
+  #content = cv2.imread(img)
   content = None
   path, name = os.path.split(img)
   metadata = ImageMetadata(path,name,IMG_SIZE,False,is_melanoma)
@@ -48,8 +53,6 @@ def label_ham_dataset(ham_filenames,ham_csv):
       if img_name in ham_filenames:
         ham_filenames[img_name].metadata.is_melanoma = label == 'mel'
         dataset_labeled.append(ham_filenames[img_name])
-      else:
-        print(img_name)
   return dataset_labeled
 
 # Consider add some test to verify the correct labeling for some random images in the dataset
@@ -75,12 +78,38 @@ def process_dataset():
   dataset = ham_dataset + melanoma_dataset
   random.shuffle(melanoma_dataset)
   return dataset
-  
-def main():
+
+def remove_duplicates(dataset):
+  uniques = {}
+  for img in dataset:
+    uniques[img.metadata.name] = img
+  dataset_uniques = list(uniques.values())
+  return dataset_uniques
+
+def divide_dataset(dataset):
+  labels = []
+  for img in dataset:
+    labels.append(int(img.metadata.is_melanoma))
+  x_train, x_test, y_train, y_test = train_test_split(dataset, labels)
+  return x_train, x_test, y_train, y_test
+
+def write_csv(dataset,filename):
+  with open(filename,'+w') as csv_file:
+    writer = csv.writer(csv_file)
+    writer.writerow(HEADER_CSV)
+    for img in dataset:
+      writer.writerow([img.metadata.name,img.metadata.path,int(img.metadata.is_melanoma)])
+
+def build_dataset():
   global dataset
   dataset = process_dataset()
-  for item in dataset:
-    print(item)
+  dataset_uniques = remove_duplicates(dataset)
+  train, test, train_label, y_label = divide_dataset(dataset_uniques)
+  write_csv(train,TRAIN_CSV_FILE)
+  write_csv(test,TEST_CSV_FILE)
 
+def main():
+  build_dataset()
+    
 if __name__ == '__main__':
   main()
