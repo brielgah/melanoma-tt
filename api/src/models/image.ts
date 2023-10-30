ba1630f589f3852dfa683ec2e4743d76cdbbd872
brielgah <gabrielgh@live.com.mx>
2023-10-30 17:23:31 -0400

    Add ORM

---
+++ api/src/models/image.ts
@@ -0,0 +1,23 @@
+import { AutoIncrement, BelongsTo, Column, CreatedAt, DeletedAt, HasMany, Model, PrimaryKey, Table, UpdatedAt } from 'sequelize-typescript';
+import {User} from './user';
+@Table
+export class Image extends Model {
+
+  @AutoIncrement
+  @PrimaryKey
+  @Column
+  id!: number;
+
+  @CreatedAt
+  creationDate?: Date;
+
+  @UpdatedAt
+  updatedOn?: Date;
+
+  @DeletedAt
+  deletionDate?: Date;
+
+  @BelongsTo(() => User)
+  user!:User;
+
+}
