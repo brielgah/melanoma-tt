9f2542c7e5a7f4a5efca385dbc3780db4cb66274
brielgah <gabrielgh@live.com.mx>
2023-11-29 20:00:37 -0600

    Fix identantion

--- backend/cnn/create_dataset.py
+++ backend/cnn/create_dataset.py
@@ -4,152 +4,172 @@ import cv2
 import csv
 import random
 from torch.utils.data import Dataset
-from image_processing.util.image import IMG_SIZE,ImageMetadata
+from image_processing.util.image import IMG_SIZE, ImageMetadata
 from sklearn.model_selection import train_test_split
 
+
 class Image():
-  
-  def __init__(self,content,metadata):
-    self.content = content
-    self.metadata = metadata
-    
-  def __str__(self):
-    return f"name: {self.metadata.name} melanoma:{'Y' if self.metadata.is_melanoma else 'N'}" 
-  
-# Constants 
+
+    def __init__(self, content, metadata):
+        self.content = content
+        self.metadata = metadata
+
+    def __str__(self):
+        return f"name: {self.metadata.name} melanoma:{'Y' if self.metadata.is_melanoma else 'N'}"
+
+
+# Constants
 DATASET_PATH = os.getenv('DATASET_PATH')
 TRAIN_CSV_FILE = os.getenv('TRAIN_CSV')
 TEST_CSV_FILE = os.getenv('TEST_CSV')
 HEADER_CSV = ["name", "path", "melanoma"]
 # Globals
 dataset = []
-not_melanoma_lesions = {'vasc':[], 'bcc':[], 'nv':[], 'df':[], 'akiec':[], 'bkl':[]}
+not_melanoma_lesions = {'vasc': [], 'bcc': [],
+                        'nv': [], 'df': [], 'akiec': [], 'bkl': []}
 
-def create_image_object(img,is_melanoma):
-  #content = cv2.imread(img)
-  content = None
-  path, name = os.path.split(img)
-  metadata = ImageMetadata(path,name,IMG_SIZE,False,is_melanoma)
-  return Image(content,metadata)
 
-def read_melanoma_dataset(files,is_melanoma):
-  images = []
-  for img in files:
-    images.append(create_image_object(img,is_melanoma))
-  return images
+def create_image_object(img, is_melanoma):
+    # content = cv2.imread(img)
+    content = None
+    path, name = os.path.split(img)
+    metadata = ImageMetadata(path, name, IMG_SIZE, False, is_melanoma)
+    return Image(content, metadata)
 
-def read_ham_dataset(ham_filenames,files):
-  for img in files:
-    _,name = os.path.split(img)
-    name = name.split('.')[0]
-    ham_filenames[name] = create_image_object(img,False)
-    
-def label_ham_dataset(ham_filenames,ham_csv):
-  dataset_labeled = []
-  with open(ham_csv,'r') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',', quotechar='|')
-    next(csv_reader)
-    for row in csv_reader:
-      img_name,label= row[1], row[2]
-      if img_name in ham_filenames:
-        ham_filenames[img_name].metadata.is_melanoma = label == 'mel'
-        dataset_labeled.append(ham_filenames[img_name])
-        if label in not_melanoma_lesions:
-          not_melanoma_lesions[label].append(ham_filenames[img_name])
-  return dataset_labeled
+
+def read_melanoma_dataset(files, is_melanoma):
+    images = []
+    for img in files:
+        images.append(create_image_object(img, is_melanoma))
+    return images
+
+
+def read_ham_dataset(ham_filenames, files):
+    for img in files:
+        _, name = os.path.split(img)
+        name = name.split('.')[0]
+        ham_filenames[name] = create_image_object(img, False)
+
+
+def label_ham_dataset(ham_filenames, ham_csv):
+    dataset_labeled = []
+    with open(ham_csv, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',', quotechar='|')
+        next(csv_reader)
+        for row in csv_reader:
+            img_name, label = row[1], row[2]
+            if img_name in ham_filenames:
+                ham_filenames[img_name].metadata.is_melanoma = label == 'mel'
+                dataset_labeled.append(ham_filenames[img_name])
+                if label in not_melanoma_lesions:
+                    not_melanoma_lesions[label].append(ham_filenames[img_name])
+    return dataset_labeled
+
 
 def get_not_melanoma_lesions():
-   train = []
-   test = []
-   SIZE = 185
-   for item,value in not_melanoma_lesions.items():
-      images = value.copy()
-      random.shuffle(images)
-      if len(images) > SIZE:
-         train += images[0:int(SIZE*0.8)]
-         test += images[int(SIZE*0.8):SIZE]
-      else:
-         train += images[0:int(len(images)*0.8)]
-         test += images[int(len(images)*0.8):len(images)]
-   return train+test
+    train = []
+    test = []
+    SIZE = 185
+    for item, value in not_melanoma_lesions.items():
+        images = value.copy()
+        random.shuffle(images)
+        if len(images) > SIZE:
+            train += images[0:int(SIZE*0.8)]
+            test += images[int(SIZE*0.8):SIZE]
+        else:
+            train += images[0:int(len(images)*0.8)]
+            test += images[int(len(images)*0.8):len(images)]
+    return train+test
 
 # Consider add some test to verify the correct labeling for some random images in the dataset
+
+
 def process_dataset():
-  ham_csv = ""
-  ham_filenames = {}
-  ham_dataset = []
-  melanoma_dataset = []
-  for root, dirs, files in os.walk(DATASET_PATH):
-    if "HAM" in root:
-      ham_paths = [os.path.join(root,filename) for filename in files]
-      read_ham_dataset(ham_filenames,ham_paths)
-    if root == DATASET_PATH:
-      for filename in files:
-        if "HAM" in filename:
-          ham_csv = os.path.join(root,filename)
-    if "skin-lesions" in root and files:
-      paths = [os.path.join(root,filename) for filename in files]
-      melanoma_dataset += read_melanoma_dataset(paths, "melanoma" in root)
-  print(f"Melanoma dataset length {len(melanoma_dataset)}")
-  ham_dataset = label_ham_dataset(ham_filenames,ham_csv)
-  print(f"HAM dataset length {len(ham_dataset)}")
-  random.shuffle(melanoma_dataset)
-  random.shuffle(ham_dataset)
-  is_melanoma = []
-  no_melanoma = []
-  for image in melanoma_dataset:
-	  if image.metadata.is_melanoma:
-		  is_melanoma.append(image)
-	  else:
-		  no_melanoma.append(image)
-  for image in ham_dataset:
-	  if image.metadata.is_melanoma:
-		  is_melanoma.append(image)
-	  else:
-		  no_melanoma.append(image)
-  return is_melanoma, no_melanoma
+    ham_csv = ""
+    ham_filenames = {}
+    ham_dataset = []
+    melanoma_dataset = []
+    for root, dirs, files in os.walk(DATASET_PATH):
+        if "HAM" in root:
+            ham_paths = [os.path.join(root, filename) for filename in files]
+            read_ham_dataset(ham_filenames, ham_paths)
+        if root == DATASET_PATH:
+            for filename in files:
+                if "HAM" in filename:
+                    ham_csv = os.path.join(root, filename)
+        if "skin-lesions" in root and files:
+            paths = [os.path.join(root, filename) for filename in files]
+            melanoma_dataset += read_melanoma_dataset(
+                paths, "melanoma" in root)
+    print(f"Melanoma dataset length {len(melanoma_dataset)}")
+    ham_dataset = label_ham_dataset(ham_filenames, ham_csv)
+    print(f"HAM dataset length {len(ham_dataset)}")
+    random.shuffle(melanoma_dataset)
+    random.shuffle(ham_dataset)
+    is_melanoma = []
+    no_melanoma = []
+    for image in melanoma_dataset:
+        if image.metadata.is_melanoma:
+            is_melanoma.append(image)
+        else:
+            no_melanoma.append(image)
+    for image in ham_dataset:
+        if image.metadata.is_melanoma:
+            is_melanoma.append(image)
+        else:
+            no_melanoma.append(image)
+    return is_melanoma, no_melanoma
+
 
 def remove_duplicates(dataset):
-  uniques = {}
-  for img in dataset:
-    uniques[img.metadata.name] = img
-  dataset_uniques = list(uniques.values())
-  return dataset_uniques
+    uniques = {}
+    for img in dataset:
+        uniques[img.metadata.name] = img
+    dataset_uniques = list(uniques.values())
+    return dataset_uniques
+
 
 def divide_dataset(dataset):
-  labels = []
-  for img in dataset:
-    labels.append(int(img.metadata.is_melanoma))
-  x_train, x_test, y_train, y_test = train_test_split(dataset, labels)
-  return x_train, x_test, y_train, y_test
-
-def write_csv(dataset,filename):
-  with open(filename,'+w') as csv_file:
-    writer = csv.writer(csv_file)
-    writer.writerow(HEADER_CSV)
-    melanoma = 0
+    labels = []
     for img in dataset:
-      row = [img.metadata.name,img.metadata.path,int(img.metadata.is_melanoma)]
-      if img.metadata.is_melanoma:
-        melanoma += 1
-      writer.writerow(row)
+        labels.append(int(img.metadata.is_melanoma))
+    x_train, x_test, y_train, y_test = train_test_split(dataset, labels)
+    return x_train, x_test, y_train, y_test
+
+
+def write_csv(dataset, filename):
+    with open(filename, '+w') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(HEADER_CSV)
+        melanoma = 0
+        for img in dataset:
+            row = [img.metadata.name, img.metadata.path,
+                   int(img.metadata.is_melanoma)]
+            if img.metadata.is_melanoma:
+                melanoma += 1
+            writer.writerow(row)
+
 
 def build_dataset():
-  global dataset
-  is_melanoma, no_melanoma = process_dataset()
-  is_melanoma = remove_duplicates(is_melanoma)
-  #no_melanoma = remove_duplicates(no_melanoma)
-  no_melanoma = get_not_melanoma_lesions()
-  print(len(is_melanoma))
-  train = is_melanoma[0:int(len(is_melanoma)*0.8)] + no_melanoma[0:int(len(no_melanoma)*0.8)]
-  test = is_melanoma[int(len(is_melanoma)*0.8):] + no_melanoma[int(len(no_melanoma)*0.8):]
-  random.shuffle(train)
-  random.shuffle(test)
-  write_csv(train,TRAIN_CSV_FILE)
-  write_csv(test,TEST_CSV_FILE)
+    global dataset
+    is_melanoma, no_melanoma = process_dataset()
+    is_melanoma = remove_duplicates(is_melanoma)
+    # no_melanoma = remove_duplicates(no_melanoma)
+    no_melanoma = get_not_melanoma_lesions()
+    print(len(is_melanoma))
+    train = is_melanoma[0:int(len(is_melanoma)*0.8)] + \
+        no_melanoma[0:int(len(no_melanoma)*0.8)]
+    test = is_melanoma[int(len(is_melanoma)*0.8):] + \
+        no_melanoma[int(len(no_melanoma)*0.8):]
+    random.shuffle(train)
+    random.shuffle(test)
+    write_csv(train, TRAIN_CSV_FILE)
+    write_csv(test, TEST_CSV_FILE)
+
 
 def main():
-  build_dataset()
-    
+    build_dataset()
+
+
 if __name__ == '__main__':
-  main()
+    main()
--- backend/cnn/data_augmentation.py
+++ backend/cnn/data_augmentation.py
@@ -14,69 +14,68 @@ TEST_DATASET_AUGMENTED_DIR = "TEST_DATASET"
 TRAIN_CSV_FILE_AUGMENTED = "train_augmented_2.csv"
 TEST_CSV_FILE_AUGMENTED = "test_augmented.csv"
 ANGLES = {45, 90, 135, 180, 225, 270, 315, 360}
-HEADER_CSV = ["file_path","melanoma"]
+HEADER_CSV = ["file_path", "melanoma"]
+
 
 def read_csv(file_path):
-  data = []
-  with open(file_path,'r') as csv_file:
-      csv_reader = csv.reader(csv_file, delimiter=',', quotechar='|')
-      next(csv_reader)
-      for row in csv_reader:
-        img_name,path,is_melanoma = row
-        data.append((os.path.join(path,img_name), is_melanoma))
-  return data
+    data = []
+    with open(file_path, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',', quotechar='|')
+        next(csv_reader)
+        for row in csv_reader:
+            img_name, path, is_melanoma = row
+            data.append((os.path.join(path, img_name), is_melanoma))
+    return data
+
 
 def verify_dir_exists():
-  output_directory = os.path.join(DATASET_PATH, TRAIN_DATASET_AUGMENTED_DIR)
-  if not os.path.exists(output_directory):
-      os.makedirs(output_directory)
-  return output_directory
+    output_directory = os.path.join(DATASET_PATH, TRAIN_DATASET_AUGMENTED_DIR)
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+    return output_directory
+
 
 def write_csv(data):
-  output_csv_path = os.path.join(DATASET_PATH,TRAIN_CSV_FILE_AUGMENTED)
-  # Write the list to the CSV file
-  with open(output_csv_path, 'w', newline='') as csvfile:
-      csv_writer = csv.writer(csvfile)
-      csv_writer.writerow(HEADER_CSV)
-      csv_writer.writerows(data)
+    output_csv_path = os.path.join(DATASET_PATH, TRAIN_CSV_FILE_AUGMENTED)
+    # Write the list to the CSV file
+    with open(output_csv_path, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(HEADER_CSV)
+        csv_writer.writerows(data)
+
 
 def main():
-   dataset_dir = verify_dir_exists()
-   data = read_csv(os.path.join(DATASET_PATH, TRAIN_CSV_FILE))
-   count = 1
-   new_images_data = []
-   for image_path, label in data:
-    file_name,file_extension = os.path.splitext(image_path)
-    _, file_name = os.path.split(file_name)
-    count += 1
-    image = ""
-    #image = cv2.imread(image_path)
-    #image,_ = process_image(image)
-    # Check if the image was successfully loaded
-    if image is not None:
-      # Get image height, width
-      #height, width = image.shape[:2]
+    dataset_dir = verify_dir_exists()
+    data = read_csv(os.path.join(DATASET_PATH, TRAIN_CSV_FILE))
+    count = 1
+    new_images_data = []
+    for image_path, label in data:
+        file_name, file_extension = os.path.splitext(image_path)
+        _, file_name = os.path.split(file_name)
+        count += 1
+        image = ""
+        image = cv2.imread(image_path)
+        image, _ = process_image(image)
+        # Check if the image was successfully loaded
+        if image is not None:
+            # Get image height, width
+            height, width = image.shape[:2]
 
-      # Define the angle of rotation (in degrees)
-      for angle in ANGLES:
-        img_name = file_name + '_' + str(angle) + file_extension
-        file_path = os.path.join(dataset_dir, img_name)
-        new_images_data.append((img_name, label))
-      # Calculate the rotation matrix
-        #rotation_matrix = cv2.getRotationMatrix2D((width / 2, height / 2), angle, 1)
-        #rotated_image = cv2.warpAffine(image, rotation_matrix, (width, height))
-        # Display the original and modified images
+            # Define the angle of rotation (in degrees)
+            for angle in ANGLES:
+                img_name = file_name + '_' + str(angle) + file_extension
+                file_path = os.path.join(dataset_dir, img_name)
+                new_images_data.append((img_name, label))
+                # Calculate the rotation matrix
+                rotation_matrix = cv2.getRotationMatrix2D(
+                    (width / 2, height / 2), angle, 1)
+                rotated_image = cv2.warpAffine(
+                    image, rotation_matrix, (width, height))
+                # Write rotated image
+                cv2.imwrite(file_path, rotated_image)
+        random.shuffle(new_images_data)
+        write_csv(new_images_data)
 
-        # Apply the rotation to the image
 
-        # Display the original and rotated images
-        #cv2.imshow('Original Image', image)
-        #cv2.imshow('Rotated Image', rotated_image)
-        #cv2.waitKey(0)
-        #cv2.destroyAllWindows()
-        #cv2.imwrite(file_path, rotated_image)
-    random.shuffle(new_images_data)
-    write_csv(new_images_data)
-    
 if __name__ == '__main__':
-  main()
+    main()
